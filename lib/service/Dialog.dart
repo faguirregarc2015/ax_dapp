0fe930e13a09b0b00fc5668baac4ca3123ae4753
Andrei Durnea <durduman@yahoo.com>
2022-07-19 19:02:59 +0300

    add dialog

--- lib/service/Dialog.dart
+++ lib/service/Dialog.dart
@@ -1857,3 +1857,76 @@ BoxDecoration boxDecoration(
       borderRadius: BorderRadius.circular(rad),
       border: Border.all(color: borCol, width: borWid));
 }
+
+class FailedDialog extends StatelessWidget {
+  const FailedDialog({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+    double wid = 500;
+    double edge = 40;
+    if (_width < 505) wid = _width;
+    double hgt = 335;
+    if (_height < 340) hgt = _height;
+
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Container(
+        height: hgt,
+        width: wid,
+        decoration: boxDecoration(Colors.grey[900]!, 30, 0, Colors.black),
+        child: Center(
+          child: Container(
+            height: 275,
+            width: wid - edge,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                    width: wid - edge,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(width: 5),
+                        Container(
+                          child: Text(
+                            "Something went wrong",
+                            style: textStyle(Colors.white, 20, false),
+                          ),
+                        ),
+                        Container(
+                          width: 40,
+                          child: IconButton(
+                            icon: const Icon(
+                              Icons.close,
+                              color: Colors.white,
+                            ),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        )
+                      ],
+                    )),
+                Expanded(
+                  child: Center(
+                    child: Icon(
+                      Icons.cancel_outlined,
+                      size: 150,
+                      color: Colors.amber[400],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
